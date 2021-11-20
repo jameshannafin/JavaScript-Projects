77ce73cfc765198642dfcfca7878c559a5d8be1e
jameshannafin <jhannafin96@gmail.com>
2021-11-20 09:22:42 -0800

    Init

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+
+// Assigning variables
+var name_1 = 'Jamis', alert_1 = "you are amazing!", blues = "I have the blues"
+    sentence_1 = "THIS string is stored as a variable.", sentence_2 = " so is this one.";
+var blues = blues.fontcolor('blue'), alert_1 = alert_1.fontcolor('red'), name_1 = name_1.fontcolor('green');
+
+// making alert pop-ups
+window.alert("you are amazing!");
+alert(alert_1);
+
+/// Writing text on screen
+document.write(blues)
+document.write('This text is written with javascript')
+document.write(name_1);
+document.write('This text is more\"difficult\" to write when coding\\ it has to use backslashes.');
+document.write('THIS string of text is concatenated  to' + ' THIS string of text.');
+document.write(sentence_1 + sentence_2);
+document.write(10*8) ///Testing an expression.
